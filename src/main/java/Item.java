7303a1b6084179632857f85030cc36e92d4740d6
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 14:54:34 +0600

    move doUpdateQuality method into Item class

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -29,5 +29,63 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    protected void doUpdateQuality() {
+        switch (name) {
+            case "Aged Brie":
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality < 50) {
+                        quality = quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
+
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    quality = 0;
+                }
+
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+
+                break;
+            default:
+                if (quality > 0) {
+                    quality = quality - 1;
+                }
+
+                sellIn = sellIn - 1;
+
+                if (sellIn < 0) {
+                    if (quality > 0) {
+                        quality = quality - 1;
+                    }
+                }
+                break;
+        }
+    }
 }
 
