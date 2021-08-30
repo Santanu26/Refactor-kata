55c7ae1cc88c8cae570a50b4d7c1b4deda944a36
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 09:31:10 +0600

    Added combinationApproval

--- src/test/java/GlideRoseTest.java
+++ src/test/java/GlideRoseTest.java
@@ -1,16 +1,26 @@
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
 
 class GildedRoseTest {
 
     @Test
     void updateQuality() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+
+        //Approvals.verify(itemString);
+        CombinationApprovals.verifyAllCombinations(
+                this:: doUpdateQuality,
+                new String[] {"foo", "Aged Brie"},
+                new Integer[] {0},
+                new Integer[] {0}
+        );
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        //assertEquals("foo", app.items[0].name);
-        Approvals.verify(app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
