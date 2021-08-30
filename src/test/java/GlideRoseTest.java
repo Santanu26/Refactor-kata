81e195b50946e629beff089605a33e0ccd9e9ef6
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 09:55:02 +0600

    Added all tests coverage

--- src/test/java/GlideRoseTest.java
+++ src/test/java/GlideRoseTest.java
@@ -1,4 +1,3 @@
-import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +6,12 @@ class GildedRoseTest {
     @Test
     void updateQuality() {
 
-        //Approvals.verify(itemString);
+        //Approvals.verify(itemString);"Backstage passes to a TAFKAL80ETC concert"
         CombinationApprovals.verifyAllCombinations(
-                this:: doUpdateQuality,
-                new String[] {"foo", "Aged Brie"},
-                new Integer[] {0},
-                new Integer[] {0}
+                this::doUpdateQuality,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1, 0, 11},
+                new Integer[]{0, 1, 49, 50}
         );
     }
 
