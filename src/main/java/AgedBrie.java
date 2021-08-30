febaab3a7c44733f3e813dc88ccc545567af56c5
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 14:59:42 +0600

    updated override methods

--- src/main/java/AgedBrie.java
+++ src/main/java/AgedBrie.java
@@ -2,4 +2,19 @@ public class AgedBrie extends Item {
     public AgedBrie(int sellIn, int quality) {
         super("Aged Brie", sellIn, quality);
     }
+
+    @Override
+    protected void doUpdateQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality = quality + 1;
+            }
+        }
+    }
 }
--- src/main/java/BackstagePassesTAFKALConcert.java
+++ src/main/java/BackstagePassesTAFKALConcert.java
@@ -2,4 +2,29 @@ public class BackstagePassesTAFKALConcert extends Item {
     public BackstagePassesTAFKALConcert( int sellIn, int quality) {
         super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
     }
+
+    @Override
+    protected void doUpdateQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+
+            if (sellIn < 11) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+
+            if (sellIn < 6) {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            quality = 0;
+        }
+    }
 }
