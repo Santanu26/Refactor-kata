59809453b0c7028dd58a489b4ae9379d5bcd7796
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 12:54:11 +0600

    convert if-else into swithch case

--- src/main/java/GlideRose.java
+++ src/main/java/GlideRose.java
@@ -14,56 +14,61 @@ class GildedRose {
     }
 
     private void doUpdateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
+        switch (item.name) {
+            case "Aged Brie":
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
-            }
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
 
-                if (item.sellIn < 11) {
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
-            }
 
-            item.sellIn = item.sellIn - 1;
+                item.sellIn = item.sellIn - 1;
 
-            if (item.sellIn < 0) {
-                item.quality = 0;
-            }
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
 
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                break;
+            case "Sulfuras, Hand of Ragnaros":
 
-        } else {
+                break;
+            default:
 
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
-            }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+                break;
         }
     }
 
