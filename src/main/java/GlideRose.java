a7d4c08c994bdbb3abeba93dc93e29f428eaf7ca
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 12:50:27 +0600

    Much refactoring

--- src/main/java/GlideRose.java
+++ src/main/java/GlideRose.java
@@ -27,13 +27,7 @@ class GildedRose {
                 }
             }
         } else {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -49,21 +43,29 @@ class GildedRose {
                         }
                     }
                 }
-            }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+
+            } else {
+                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
+                } else {
+
                     if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
+                    }
+
+                    item.sellIn = item.sellIn - 1;
+
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
                             item.quality = item.quality - 1;
                         }
                     }
-                } else {
-                    item.quality = 0;
                 }
             }
         }
