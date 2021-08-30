a4d9969414f278e7042348a1f2d71b65a752bfc3
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 15:25:06 +0600

    Updated refactoring with switch statement to  polymorphic way

--- src/main/java/GlideRose.java
+++ src/main/java/GlideRose.java
@@ -8,15 +8,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            items[i].doUpdateQuality();
+        for (Item item : items) {
+            item.doUpdateQuality();
         }
-    }
-
-    @Override
-    public String toString() {
-        return "GildedRose{" +
-                "items=" + Arrays.toString(items) +
-                '}';
     }
 }
