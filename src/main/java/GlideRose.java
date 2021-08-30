756ac44e85939f58bd00bc9bb39b40d276fcd659
shantanu <shantanu.barua@vivacomsolutions.com>
2021-08-30 12:52:40 +0600

    Merge else-if

--- src/main/java/GlideRose.java
+++ src/main/java/GlideRose.java
@@ -26,46 +26,42 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
             }
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0) {
+                item.quality = 0;
+            }
+
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+
         } else {
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
 
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
+            item.sellIn = item.sellIn - 1;
 
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-
-            } else {
-                if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-
-                } else {
-
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-
-                    item.sellIn = item.sellIn - 1;
-
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
+            if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
